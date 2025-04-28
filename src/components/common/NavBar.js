8108092be9fa4b3912bff0d06edcdfca2c98d25e
Hwan Yoo <isacyoo@gmail.com>
2025-04-28 15:40:51 +1200

    typo NavBar

--- src/components/common/NavBar.js
+++ src/components/common/NavBar.js
@@ -8,19 +8,19 @@ export default function NavBar() {
             <Link href ='/' className='ml-7 text-sm transition-all'>
                 Home
             </Link>
-            <div className='h-7 w-0.5 border-l-2 border-l-primary-900 ml-7'></div>
+            <div className='h-7 w-0.5 border-l-2 border-l-primary/20 mx-7'></div>
             <div className='flex items-center ml-auto mr-2'>
                 <ModeToggle />
-                <div className='h-7 w-0.5 border-l-2 border-l-primary-900 ml-7'></div>
+                <div className='h-7 w-0.5 border-l-2 border-l-primary/20 mx-7'></div>
                 <PagesDropdown />
-                <div className='h-7 w-0.5 border-l-2 border-l-primary-900 ml-7'></div>
-                <Link href='/api-reference' className='ml-7 border-'>API Reference</Link>
-                <div className='h-7 w-0.5 border-l-2 border-l-primary-900 ml-7'></div>
-                <Link href='/architecture' className='ml-7 border-'>Architecture</Link>
-                <div className='h-7 w-0.5 border-l-2 border-l-primary-900 ml-7'></div>
-                <Link href='/erd' className='ml-7 border-'>ERD</Link>
-                <div className='h-7 w-0.5 border-l-2 border-l-primary-900 ml-7'></div>
-                <Link href='/demo' className='ml-7 border-'>Start Demo</Link>                           
+                <div className='h-7 w-0.5 border-l-2 border-l-primary/20 mx-7'></div>
+                <Link href='/api-reference'>API Reference</Link>
+                <div className='h-7 w-0.5 border-l-2 border-l-primary/20 mx-7'></div>
+                <Link href='/architecture'>Architecture</Link>
+                <div className='h-7 w-0.5 border-l-2 border-l-primary/20 mx-7'></div>
+                <Link href='/erd'>ERD</Link>
+                <div className='h-7 w-0.5 border-l-2 border-l-primary/20 mx-7'></div>
+                <Link href='/demo'>Start Demo</Link>                           
             </div>
         </div>
     )
